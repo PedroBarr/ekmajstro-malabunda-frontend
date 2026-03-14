440f98f9771130c1c80ff24bb1449dacb4969f60
Pedro Barr <pebarreraa@udistrital.edu.co>
2026-03-14 04:02:13 -0500

    Agregar ajustes para el despliegue

--- manage.py
+++ manage.py
@@ -1,14 +1,3 @@
-#-------------------------------------------------------------------------------
-# Name:        Launcher
-# Purpose:     Lanza la aplicacion
-#
-# Author:      Aref
-#
-# Created:     1/9-7/2023
-# Copyright:   (R) ACMUD 2022 / (k) Alta Lengua 2023 --
-# Licence:     <GPLv3>
-#-------------------------------------------------------------------------------
-
 import src.main
 
 def main(): src.main.main()

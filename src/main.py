ddac777a3fa2f6d8f6a9306b2afa75f3c607b421
Pedro Barr <pebarreraa@udistrital.edu.co>
2026-03-14 03:04:34 -0500

    Agregar enrutador

--- src/main.py
+++ src/main.py
@@ -1,6 +1,30 @@
 import flet as ft
 
-def index(page: ft.Page): pass
+def index(pagina: ft.Page):
+    pagina.title = "Malabunda"
+    pagina.theme_mode = ft.ThemeMode.DARK
+    
+    def enrutador(e):
+        pagina.views.clear()
+
+        if pagina.route == "/":
+            pagina.views.append(
+                ft.View(
+                    route="/",
+                    controls=[ft.Text(value="Home", size=35)]
+                )
+            )
+
+        pagina.update()
+
+    def pinchar_vista(e):
+        if len(pagina.views) > 1:
+            pagina.views.pop()
+            pagina.go(pagina.views[-1].route)
+
+    pagina.on_route_change = enrutador
+    pagina.on_view_pop = pinchar_vista
+    enrutador(None)
 
 def main(): ft.app(target=index)
 

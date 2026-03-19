bbfef8ae18ac23d58b9e015201502eaa5fa783ca
Pedro Barr <pebarreraa@udistrital.edu.co>
2026-03-19 00:43:04 -0500

    Modificar modelo persona

--- src/models/persona.py
+++ src/models/persona.py
@@ -10,8 +10,11 @@
 # Licencia:    <GPLv3>
 #-------------------------------------------------------------------------------
 
+import flet as ft
+
 from pydantic import BaseModel, Field
 from typing import Optional, Dict, Any
+from datetime import datetime
 
 class Persona(BaseModel):
     """ Clase: Persona
@@ -20,10 +23,62 @@ class Persona(BaseModel):
     Modelo de datos para representar a una persona para su
     serialización y deserialización en la aplicación.
     """
-    id: str = Field(..., alias="_id")
-    nombre: Optional[str]
+    id: Optional[str] = None
+    nombre: Optional[str] = ""
     apellido: str
     metadatos: Optional[Dict[str, Any]] = \
         Field(default_factory=dict, alias="metadata")
+    modificado: Optional[datetime] = \
+        Field(default_factory=datetime.now, alias="updatedAt")
     
-    class ConfigDict: populate_by_name = True
+    model_config = {"populate_by_name": True,}
+
+    def imagen(self) -> ft.Control:
+        imagen = self.metadatos.get("imagen")
+        iniciales = (
+            (self.nombre[0] if self.nombre else "") +
+            (self.apellido[0] if self.apellido else "")
+        )
+
+        guardaespacio = "?" if not iniciales else iniciales.upper()
+
+        if imagen:
+            return ft.Image(src=imagen, width=50, height=50, fit=ft.ImageFit.COVER)
+        else:
+            return ft.Text(
+                guardaespacio,
+                size=25,
+                weight=ft.FontWeight.BOLD,
+                color=ft.Colors.WHITE,
+            )
+        
+    def marca_tiempo_modificacion(self) -> str:
+        if not self.modificado: return ""
+        return self.modificado.strftime("%Y-%m-%d %H:%M:%S")
+        
+    def modificar(self, dicc: Dict[str, Any]):
+        for clave, valor in dicc.items():
+            if hasattr(self, clave): setattr(self, clave, valor)
+
+    @classmethod
+    def sintetizar(
+        cls,
+        id: Optional[str] = None,
+        nombre: Optional[str] = "",
+        apellido: str = "",
+        metadatos: Optional[Dict[str, Any]] = None,
+        modificado: Optional[datetime] = None,
+        dicc: Optional[Dict[str, Any]] = None,
+    ):
+        _id = id or (dicc.get("id") if dicc else None)
+        _nombre = nombre or (dicc.get("nombre") if dicc else "")
+        _apellido = apellido or (dicc.get("apellido") if dicc else "")
+        _metadatos = metadatos or (dicc.get("metadata") if dicc else None) or {}
+        _modificado = modificado or (dicc.get("updatedAt") if dicc else None) or datetime.now()
+        return cls(
+            id=_id,
+            nombre=_nombre,
+            apellido=_apellido,
+            metadatos=_metadatos,
+            modificado=_modificado,
+        )
